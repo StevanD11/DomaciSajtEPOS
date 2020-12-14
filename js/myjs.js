5228aa4b8e8061902e393a3da7bc5a36f15007e5
Stevan Draskovic <drasko1212@gmail.com>
2020-12-14 02:22:36 +0100

    Inicijalni commit

---
+++ js/myjs.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $('.sharetastic').sharetastic();
+
+    $('form').submit(function () {
+        let errors = false;
+        var ime = $('#ime').val();
+        var email = $('#email').val();
+        var naslov = $('#naslov').val();
+        var poruka = $('#poruka').val();
+        var robot = $('input[name=robot]:checked');
+
+         if (robot.length == 0) {
+            errors = true;
+             $('.errors').html('Molimo vas da odgovorite na pitanje!');
+        }
+
+        if (poruka == '') {
+            errors = true;
+            $('.errors').html('Molimo vas da napišete poruku!');
+        }
+
+        if (naslov == '') {
+            errors = true;
+            $('.errors').html('Molimo vas da unesete naslov poruke!');
+        }
+
+        if (email == '') {
+            errors = true;
+            $('.errors').html('Molimo vas da unesete e-mail adresu');
+        }
+
+        if (ime == '') {
+            errors = true;
+            $('.errors').html('Molimo vas da unesete vaše ime');
+        }
+
+        if (errors == true) {
+            return false;
+        } 
+
+        if (errors == false) {
+            $('.errors').html('');
+            let d = new Date();
+            alert('Uspesno popunjena forma dana  ' + (d.toLocaleString('en-AU'
+            )));
+            return true;
+        }
+        
+    });
+    
+
+
+
+
+
+});
